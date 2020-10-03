71bd2df36833a39e15268d5c7f2ca90a544a796f
Aurino Geraldo <aurinoaj@gmail.com>
2020-10-03 00:58:21 -0300

    Adding date format

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const pasto = [
-  {
-    nick: 'Aurigod',
-  },
-  {
-    nick: 'Kevin',
-  }
-]
+const pasto = []
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`)
 });
 
 client.on('message', msg => {
   if(!msg.guild) return
 
+  if(msg.content.startsWith('!gado')) {
+    const user = msg.mentions.users.first()
+
+    if(user) {
+      const member = msg.guild.member(user)
+      const dataAtual = new Date()
+    
+      if(member) {
+        pasto.push({
+          nick: member.user.username,
+          entrada: formataData(dataAtual),
+          saida: calculaSaida(dataAtual)
+        })
+      }
+    }
+  }
+
   if(msg.content === '!pasto') {
-    msg.reply('Pastando no momento:')
+    msg.channel.send('Pastando no momento:')
     pasto.map((gado) => {
-      msg.channel.send(`${gado.nick}`)
+      msg.channel.send(`Boi: ${gado.tag}`)
+      msg.channel.send(`Entrou no pasto em ${gado.entrada}`)
+      msg.channel.send(`Pastando até ${gado.saida}`)
+      msg.channel.send(`--------------------`)
     })
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+function formataData(data) {
+  return `${data.getDate()}.${data.getMonth()+1}.${data.getFullYear()}`
+}
+
+function calculaSaida(dataEntrada){
+  dataEntrada.setDate(dataEntrada.getDate()+7)
+  const dataSaidaFormatada = formataData(dataEntrada)
+  return dataSaidaFormatada
+}
+
+client.login(process.env.BOT_TOKEN)
